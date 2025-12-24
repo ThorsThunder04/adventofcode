8e7fd5cc0caaef9eb787452c851795d5c6635fc1
ThorsThunder04 <87436831+ThorsThunder04@users.noreply.github.com>
2025-12-25 00:23:42 +0100

    FINISHED 4 part 1

--- cpp/2025/4_1.cpp
+++ cpp/2025/4_1.cpp
@@ -8,23 +8,28 @@ using namespace std;
 int hm_adj(vector<string> &grid, int r, int c) {
     int rows = (int)grid.size();
     int cols = (int)grid[0].size();
+    char tc = '@';
 
     int num_adj_rolls = 0;
 
-    // just me complicating my life by not hardcoding this
-    int ros = -1, cos = -1, roe = 1, coe = 1;
+    // I decided to hard code this just cuz I know that it will defo work like this
+    // My iterative version felt a little overcomplicated anyway tbh
 
-    if (r == 0) ros = 0;
-    if (r == rows-1) roe = 0;
-    if (c == 0) cos = 0;
-    if (c == cols-1) coe = 0;
-
-    for (int rr = r - ros; rr < r + roe; rr++) {
-        for (int cc = c - cos; cc < c + coe; cc++) {
-            cout << grid[rr][cc];
-            if (rr != r && cc != c && grid[rr][cc] == '@') num_adj_rolls++;
-        }
+    if (r > 0) {
+        if (c > 0 && grid[r-1][c-1] == tc) num_adj_rolls++;
+        if (c < cols-1 && grid[r-1][c+1] == tc) num_adj_rolls++;
+        if (grid[r-1][c] == tc) num_adj_rolls++;
     }
+
+    if (c > 0 && grid[r][c-1] == tc) num_adj_rolls++;
+
+    if (r < rows-1) {
+        if (c > 0 && grid[r+1][c-1] == tc) num_adj_rolls++;
+        if (c < cols-1 && grid[r+1][c+1] == tc) num_adj_rolls++;
+        if (grid[r+1][c] == tc) num_adj_rolls++;
+    }
+
+    if (c < cols-1 && grid[r][c+1] == tc) num_adj_rolls++;
 
     return num_adj_rolls;
 }
@@ -39,11 +44,11 @@ int main() {
     }
 
     int num_accessible = 0;
-    cout <<roll_grid.size() << " " << roll_grid[0].size() << endl;
     for (int r = 0; r < (int)roll_grid.size(); r++) {
         for (int c = 0; c < (int)roll_grid[r].size(); c++) {
 
-            if (hm_adj(roll_grid, r, c) < 4) {
+            if (roll_grid[r][c] == '@' && hm_adj(roll_grid, r, c) < 4) {
+
                 num_accessible++;
             }
         }
