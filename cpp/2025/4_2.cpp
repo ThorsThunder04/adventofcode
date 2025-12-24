80410923800142b8a2c33c961bd25f86d40e3661
ThorsThunder04 <87436831+ThorsThunder04@users.noreply.github.com>
2025-12-25 00:37:28 +0100

    finished Day4 part 2

---
+++ cpp/2025/4_2.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+
+using namespace std;
+
+
+int hm_adj(vector<string> &grid, int r, int c) {
+    int rows = (int)grid.size();
+    int cols = (int)grid[0].size();
+    char tc = '@';
+
+    int num_adj_rolls = 0;
+
+    // I decided to hard code this just cuz I know that it will defo work like this
+    // My iterative version felt a little overcomplicated anyway tbh
+
+    if (r > 0) {
+        if (c > 0 && grid[r-1][c-1] == tc) num_adj_rolls++;
+        if (c < cols-1 && grid[r-1][c+1] == tc) num_adj_rolls++;
+        if (grid[r-1][c] == tc) num_adj_rolls++;
+    }
+
+    if (c > 0 && grid[r][c-1] == tc) num_adj_rolls++;
+
+    if (r < rows-1) {
+        if (c > 0 && grid[r+1][c-1] == tc) num_adj_rolls++;
+        if (c < cols-1 && grid[r+1][c+1] == tc) num_adj_rolls++;
+        if (grid[r+1][c] == tc) num_adj_rolls++;
+    }
+
+    if (c < cols-1 && grid[r][c+1] == tc) num_adj_rolls++;
+
+    return num_adj_rolls;
+}
+
+int main() {
+
+    vector<string> roll_grid;
+    vector<array<int,2>> to_remove; // for some reason my compiler doesn't detect the pair class, so I'm using arrays
+    int i = 0;
+
+    string line;
+    while (getline(cin, line)) {
+        roll_grid.push_back(line);
+    }
+
+    int num_accessible;
+    do {
+        num_accessible = 0;
+        for (int r = 0; r < (int)roll_grid.size(); r++) {
+            for (int c = 0; c < (int)roll_grid[r].size(); c++) {
+
+                if (roll_grid[r][c] == '@' && hm_adj(roll_grid, r, c) < 4) {
+                    num_accessible++;
+                    to_remove.push_back((array<int,2>){r,c});
+                }
+            }
+        }
+
+        while (i < (int)to_remove.size()) {
+            roll_grid[to_remove[i][0]][to_remove[i][1]] = '.';
+            i++;
+        }
+
+    } while (num_accessible > 0);
+
+    cout << (int)to_remove.size();
+
+    return 0;
+}
